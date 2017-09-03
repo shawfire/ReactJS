84a540a141bce71a355237055c7cef7cc14aff6b
John Shaw <shaw_fire@yahoo.com>
2017-09-03 19:28:25 +1000

    Add react-icons to Alert1

--- Essentials/src/components/Alert1.js
+++ Essentials/src/components/Alert1.js
@@ -1,9 +1,13 @@
 import { Component } from 'react'
+import InfoCircle from 'react-icons/lib/fa/info-circle'
+import Bomb from 'react-icons/lib/fa/bomb'
 
 export class Alert1 extends Component {
   render() {
     return (
       <div className={`alert alert-${this.props.alertType}`} role="alert">
+        {/*{(alertType == "info") ? <InfoCircle/> : ""}
+        {(alertType == "danger") ? <Bomb/> : ""}*/}
         {this.props.children}
       </div>
     )
